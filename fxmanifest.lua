9140b934abc9344560b891fceb7c8d54bbbd600c
S1lentcodes <Silentcodes@protonmail.com>
2023-01-04 21:08:46 -0500

    Better organization of files

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,21 @@
 fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
 
 description 'svelte-template'
 version '0.0.1'
 
-client_script 'client.lua'
-server_script 'server.lua'
-lua54 'yes'
+shared_scripts {
+	'config.lua'
+}
+
+client_scripts {
+	'client/*.lua',
+}
+
+server_scripts {
+	'server/*.lua'
+}
 
 ui_page 'html/index.html'
 
